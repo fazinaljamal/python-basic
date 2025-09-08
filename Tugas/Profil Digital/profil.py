98e6c58d02f50821a0865901a60d859c16fa1ba5
fazinaljamal <fazinhanzala@gmail.com>
2025-09-08 20:53:33 +0700

    menambahkan file

---
+++ Tugas/Profil Digital/profil.py
@@ -0,0 +1,45 @@
+# profil digital fazin hanzala
+
+print("=====profil digital anda=====")
+nama = input("nama: ")
+kelas = input("KELAS: ")
+hobi = input("hobi: ")
+cita_cita = input("cita-cita: ")
+belajar = input("belajar paling enak pas: ")
+tahun_lahir = input("tahun lahir: ")
+
+# ===tipe digital===
+
+print("pilih tipe (ketik angkanya saja):")
+print("1. Wibu (bau bawang)")
+print("2. Gamer (emang beneran gamer)")
+print("3. K-Popers (sok sok an korea)")
+print("4. Anak Konten (konten kreator )")
+print("5. Anak Nongki (Yang penting kumpul bos)")
+type = input("type yang kamu mana fazin ganteng: ")
+
+# pertanyaan tambahan dari mas hamka , mas daffa dan pak andi yang baik banget.
+if type == "1":
+    print("ngapain sih jadi wibu,bau bawang lu .")
+elif type == "2" : 
+    print("anak gamer banget ni.")
+elif type == "3" :
+    print("ngapain sih suka k-popers gitu, sok sok an korea lagi.")
+elif type == "4" :
+    print("wisss, konten kreator, banyak duit dong.")
+elif type == "5" :
+    print("yang penting nongki yak, walau pun gak ada duit.")
+else:
+    print("pilih 1-5 doang boy.")
+# === fun chek ===
+
+print("===fun chek===")
+
+bau = (input("teman sebelah bau ya? (iya/tidak): "))
+if bau.lower() == "iya" :
+    print("ya emang gw wangi, gimana siii")
+
+else:
+    print("pakai pewangi dong atau rexona gitu.")
+
+print("===================================")
