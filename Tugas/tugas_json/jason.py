b73eab56005f972a8b2bc03c901bd9ba190968c3
fazinaljamal <fazinhanzala@gmail.com>
2025-09-08 20:45:04 +0700

    nambahkan file

--- Tugas/tugas_json/jason.py
+++ Tugas/tugas_json/jason.py
@@ -1,27 +1,16 @@
-import json
+import requests
 
-# materi 11 kelas A dengan json
+url = "https://api.aladhan.com/v1/timingsByCity?city=Jakarta&country=Indonesia&method=20"
 
-# tambahkan module `json`` dengan `import`
-print("Materi 11 - JASON IS THE BEST")
-print("-------- RED JSON  -----------")
+response = requests.get(url)
+print("Status code:", response.status_code)   # harusnya 200 kalau sukses
 
-file_path_json = r"C:\Jamal new\New folder\materi.10\tugas_json\materi.json"
-# siapkan data  kosong 
-# 
-# 1. Baca seluruh data
-with open(file_path_json, "r") as file_materi:
-  # csv.reader() -> membaca isi file csv
-  data_materi = json.load(file_materi)
-   #akses data json dengan 'key' nya 
-    
-  print(f"judul berkas: {data_materi['title']}")
-  print(f"total kelas A: {data_materi['total']}")
-  print(f"status_santri: {data_materi['status_santri']}")
-  print(f"status_kelulusan: {data_materi['status_santri']}")
-  for pelajaran in data_materi ['pelajaran']:
-    print(f"---> {pelajaran}")
+data_json = response.json()
+print("Data keys:", data_json.keys())         # cek struktur JSON
 
-  print("| nama | surah['no'] | surah['nama'] | surah['turun'] | surah['ayat]")
-  for surah in data_materi['surah'] :
-    print (f"| { surah['no']} | { surah['nama']} | {surah['turun']} | {surah['ayat']}")
+tanggal = data_json['data']['date']['readable']
+jadwal = data_json['data']['timings']
+
+print("Tanggal:", tanggal)
+for sholat, waktu in jadwal.items():
+    print(f"{sholat:10} : {waktu}")
